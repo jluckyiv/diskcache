b3b907b0184c4ce2f2f30e1a3abf7bf566dba4d2
Jackson Lucky <jack@jacksonlucky.net>
2024-05-11 19:49:28 -0700

    First implementation.

---
+++ diskcache.go
@@ -0,0 +1,129 @@
+package diskcache
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"time"
+)
+
+type Cache struct {
+	dir string
+}
+
+type data struct {
+	Expiry time.Time
+	Key    string
+	Value  []byte
+}
+
+func New(dir string) (Cache, error) {
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return Cache{}, err
+	}
+	return Cache{
+		dir: dir,
+	}, nil
+}
+
+func (c Cache) Dir() string {
+	return c.dir
+}
+
+func (c Cache) Filename(key string) string {
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(key)))
+}
+
+func (c Cache) Path(key string) string {
+	return path.Join(c.dir, c.Filename(key))
+}
+
+func (c Cache) Put(key string, value []byte, duration time.Duration) error {
+	bytes, err := json.Marshal(data{
+		Key:    key,
+		Value:  value,
+		Expiry: time.Now().Add(duration),
+	})
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(c.Path(key), bytes, 0644)
+}
+
+func (c Cache) Get(key string) ([]byte, error) {
+	d, err := c.Load(key)
+	if err != nil {
+		return nil, err
+	}
+	if time.Now().After(d.Expiry) {
+		return nil, fmt.Errorf("cache expired")
+	}
+	return d.Value, nil
+}
+
+func (c Cache) Expiry(key string) time.Time {
+	d, err := c.Load(key)
+	if err != nil {
+		return time.Time{}
+	}
+	return d.Expiry
+}
+
+func (c Cache) IsExpired(key string) bool {
+	return time.Now().After(c.Expiry(key))
+}
+
+func (c Cache) Load(key string) (data, error) {
+	bytes, err := os.ReadFile(c.Path(key))
+	if err != nil {
+		return data{}, err
+	}
+	var d data
+	err = json.Unmarshal(bytes, &d)
+	if err != nil {
+		return data{}, err
+	}
+	return d, nil
+}
+
+func (c Cache) Remove(key string) error {
+	return os.Remove(c.Path(key))
+}
+
+func (c Cache) Clear() error {
+	files, err := os.ReadDir(c.dir)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		err = os.Remove(path.Join(c.dir, file.Name()))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c Cache) List() ([]string, error) {
+	files, err := os.ReadDir(c.dir)
+	if err != nil {
+		return nil, err
+	}
+	var keys []string
+	for _, file := range files {
+		bytes, err := os.ReadFile(path.Join(c.dir, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		var d data
+		err = json.Unmarshal(bytes, &d)
+		if err != nil {
+			return nil, err
+		}
+		keys = append(keys, d.Key)
+	}
+	return keys, nil
+}
---
+++ diskcache_test.go
@@ -0,0 +1,231 @@
+package diskcache_test
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/jluckyiv/diskcache"
+)
+
+func TestDiskCache(t *testing.T) {
+	tempdir := t.TempDir()
+	cacheFolder := "testcache"
+	cacheDir := path.Join(tempdir, cacheFolder)
+	cache, err := diskcache.New(cacheDir)
+	if err != nil {
+		t.Fatalf("Error creating cache: %v", err)
+	}
+	if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+		t.Fatalf("Cache dir %s does not exist", cache.Dir())
+	}
+	if cache.Dir() != cacheDir {
+		t.Fatalf("Want cache dir to be %s, got %s", cacheDir, cache.Dir())
+	}
+
+	t.Run("TestFilename", func(t *testing.T) {
+		key := "testkey"
+		got := cache.Filename(key)
+		want := fmt.Sprintf("%x", sha256.Sum256([]byte(key)))
+		if got != want {
+			t.Fatalf("Want filename to be %s, got %s", want, got)
+		}
+	})
+
+	t.Run("TestPath", func(t *testing.T) {
+		key := "testkey"
+		got := cache.Path(key)
+		filename := fmt.Sprintf("%x", sha256.Sum256([]byte(key)))
+		want := path.Join(cacheDir, filename)
+		if got != want {
+			t.Fatalf("Want cache path to be %s, got %s", want, got)
+		}
+	})
+
+	t.Run("TestData", func(t *testing.T) {
+		key := "testkey"
+		want := []byte("testvalue")
+		err := cache.Put(key, want, 1*time.Minute)
+		if err != nil {
+			t.Fatalf("Error saving cache: %v", err)
+		}
+		got, err := cache.Get(key)
+		if err != nil {
+			t.Fatalf("Error getting cache: %v", err)
+		}
+		if string(got) != string(want) {
+			t.Fatalf("Expected cache value to be %s, got %s", string(want), string(got))
+		}
+		data, err := cache.Load(key)
+		if err != nil {
+			t.Fatalf("Error loading cache: %v", err)
+		}
+		if string(data.Value) != string(want) {
+			t.Fatalf("Expected cache value to be %s, got %s", want, data.Value)
+		}
+		if data.Key != key {
+			t.Fatalf("Expected cache key to be %s, got %s", key, data.Key)
+		}
+		if data.Expiry.IsZero() {
+			t.Fatalf("Expected cache expiry to be non-zero")
+		}
+		if data.Expiry.Before(time.Now()) {
+			t.Fatalf("Expected cache expiry to be in the future")
+		}
+		if data.Expiry.After(time.Now().Add(1 * time.Minute)) {
+			t.Fatalf("Expected cache expiry to be within 1 minute")
+		}
+	})
+
+	t.Run("TestUnexpiredCache", func(t *testing.T) {
+		key := "unexpired"
+		err := cache.Put(key, []byte(""), 1*time.Minute)
+		if err != nil {
+			t.Fatalf("Error saving cache: %v", err)
+		}
+		_, err = cache.Get(key)
+		if err != nil {
+			t.Fatalf("Error getting cache: %v", err)
+		}
+		expiry := cache.Expiry(key)
+		if expiry.IsZero() {
+			t.Fatalf("Expected cache expiry to be non-zero")
+		}
+		if expiry.Before(time.Now()) {
+			t.Fatalf("Expected cache expiry to be in the future")
+		}
+		if expiry.After(time.Now().Add(1 * time.Minute)) {
+			t.Fatalf("Expected cache expiry to be within 1 minute")
+		}
+		isExpired := cache.IsExpired(key)
+		if isExpired {
+			t.Fatalf("Expected cache to not be expired")
+		}
+	})
+
+	t.Run("TestExpiredCache", func(t *testing.T) {
+		key := "expired"
+		err := cache.Put(key, []byte(""), -1*time.Minute)
+		if err != nil {
+			t.Fatalf("Error saving cache: %v", err)
+		}
+		_, err = cache.Get(key)
+		if err == nil {
+			t.Fatalf("Expected error getting cache")
+		}
+		if err.Error() != "cache expired" {
+			t.Fatalf("Expected error message to be 'cache expired', got %s", err.Error())
+		}
+		isExpired := cache.IsExpired(key)
+		if !isExpired {
+			t.Fatalf("Expected cache to be expired")
+		}
+	})
+
+	t.Run("TestUpdate", func(t *testing.T) {
+		key := "testkey"
+		oldvalue := []byte("oldvalue")
+		err := cache.Put(key, oldvalue, 1*time.Minute)
+		if err != nil {
+			t.Fatalf("Error saving cache: %v", err)
+		}
+		got, err := cache.Get(key)
+		if err != nil {
+			t.Fatalf("Error getting cache: %v", err)
+		}
+		if string(got) != string(oldvalue) {
+			t.Fatalf("Expected cache value to be %s, got %s", string(oldvalue), string(got))
+		}
+		newvalue := []byte("newvalue")
+		err = cache.Put(key, newvalue, 1*time.Minute)
+		if err != nil {
+			t.Fatalf("Error saving cache: %v", err)
+		}
+		got, err = cache.Get(key)
+		if err != nil {
+			t.Fatalf("Error getting cache: %v", err)
+		}
+		if string(got) != string(newvalue) {
+			t.Fatalf("Expected cache value to be %s, got %s", string(newvalue), string(got))
+		}
+	})
+
+	t.Run("TestRemove", func(t *testing.T) {
+		key := "delete"
+		err := cache.Put(key, []byte("value"), 1*time.Minute)
+		if err != nil {
+			t.Fatalf("Error saving cache: %v", err)
+		}
+		_, err = cache.Get(key)
+		if err != nil {
+			t.Fatalf("Error getting cache: %v", err)
+		}
+		err = cache.Remove(key)
+		if err != nil {
+			t.Fatalf("Error deleting cache: %v", err)
+		}
+		_, err = cache.Get(key)
+		if err == nil {
+			t.Fatalf("Expected error getting cache")
+		}
+	})
+
+	t.Run("TestList", func(t *testing.T) {
+		// Clear the cache.
+		err := cache.Clear()
+		if err != nil {
+			t.Fatalf("Error clearing cache: %v", err)
+		}
+
+		empty, err := cache.List()
+		if err != nil {
+			t.Fatalf("Error listing cache: %v", err)
+		}
+		if len(empty) != 0 {
+			t.Fatalf("Expected 0 keys, got %d", len(empty))
+		}
+
+		// Save some test data.
+		testData := []struct {
+			key   string
+			value string
+		}{
+			{"key1", "value1"},
+			{"key2", "value2"},
+			{"key3", "value3"},
+		}
+
+		for _, td := range testData {
+			err := cache.Put(td.key, []byte(td.value), 1*time.Minute)
+			if err != nil {
+				t.Fatalf("Error saving cache: %v", err)
+			}
+		}
+
+		// List the keys.
+		keys, err := cache.List()
+		if err != nil {
+			t.Fatalf("Error listing cache: %v", err)
+		}
+		if len(keys) != 3 {
+			t.Fatalf("Expected 3 keys, got %d", len(keys))
+		}
+
+		// Check that all the test data keys are in the list.
+		for _, td := range testData {
+			found := false
+			for _, key := range keys {
+				if key == td.key {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Fatalf("Expected key %s to be in list", td.key)
+			}
+		}
+	})
+}
